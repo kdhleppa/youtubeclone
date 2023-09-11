c9c8c7fb969fbc07c91a1664570eec0765ae3d2c
kdhleppa <newkdh8624@gmail.com>
2023-09-11 18:36:56 +0900

    0911

---
+++ semi/semi2.js
@@ -0,0 +1,69 @@
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };  
+
+// 지도를 생성합니다    
+var map = new kakao.maps.Map(mapContainer, mapOption); 
+
+// 주소-좌표 변환 객체를 생성합니다
+var geocoder = new kakao.maps.services.Geocoder();
+
+var previousMarker = null;
+var previousInfowindow = null;
+
+document.getElementById("btn").addEventListener("click", function(){
+    const textbox = document.getElementById("textbox")
+    
+    geocoder.addressSearch(textbox.value, function(result, status) {
+        
+
+
+        // 정상적으로 검색이 완료됐으면 
+         if (status === kakao.maps.services.Status.OK) {
+            removePreviousMarker();
+            var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+    
+            // 결과값으로 받은 위치를 마커로 표시합니다
+            
+            var marker = new kakao.maps.Marker({
+                map: map,
+                position: coords
+            });
+    
+            // 인포윈도우로 장소에 대한 설명을 표시합니다
+            var infowindow = new kakao.maps.InfoWindow({
+                content: '<div id="div1" style="width:150px;text-align:center;padding:6px 0;">현재주소</div>'
+            });
+            infowindow.open(map, marker);
+    
+            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+            map.setCenter(coords);
+            previousMarker = marker;
+            previousInfowindow = infowindow;
+        } 
+        
+    });
+
+    console.log((textbox.value));
+
+
+});
+
+
+
+// 이전에 생성한 마커 객체를 저장하는 변수
+
+// 이전 마커를 삭제하는 함수
+function removePreviousMarker() {
+    if (previousMarker !== null) {
+        previousInfowindow.close();
+
+        previousMarker.setMap(null);
+        
+       
+    }
+}
+
+
